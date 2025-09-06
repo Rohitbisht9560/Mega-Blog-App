a30abf7722f68e0aeddc093f9928567dc609453b
Rohit Bisht <Rohitbisht9560@gmail.com>
2025-09-06 12:30:22 +0530

    AppWrite Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-      <h1>Blog Appwrite</h1>
+      <h1>A Blog Appwrite</h1>
     </>
   );
 }
