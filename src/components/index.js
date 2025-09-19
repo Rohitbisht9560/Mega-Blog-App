14b026026aa1b273fae50eda8b287e1f57588325
Rohit Bisht <Rohitbisht9560@gmail.com>
2025-09-19 14:38:02 +0530

    Adding new components and React hook form

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
-export {Header, Footer,Container, LogoutBtn, Logo};
+import Button from "./Button";
+import Input from "./Input";
+export {Header, Input, Footer,Container, LogoutBtn, Logo, Button,};
