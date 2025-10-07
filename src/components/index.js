9c3dd194432e40f5b21b4659820fed097f841e1e
Rohit Bisht <Rohitbisht9560@gmail.com>
2025-10-07 16:58:40 +0530

    Fix in Index.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container"
 import LogoutBtn from "./Header/LogoutBtn";
 import Login from "./Login";
 import Logo from "./Logo";
